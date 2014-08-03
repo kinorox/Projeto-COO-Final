64bbef0929182aed89f57e468b019c1e2c21c019
Gabriel <gabriel.bombardi@gmail.com>
2014-08-03 14:42:50 -0300

    5th commit
    shootemup facade delete

--- src/std/ep/game/elements/colide/Colide.java
+++ src/std/ep/game/elements/colide/Colide.java
@@ -2,6 +2,15 @@ package std.ep.game.elements.colide;
 
 public class Colide {
 	
+	public boolean playerProjectileCollision(){
+		return false;
+	}
 	
+	public boolean playerEnemyCollision(){
+		return false;
+	}
 	
+	public boolean playerProjectileEnemyCollision(){
+		return false;
+	}
 }
